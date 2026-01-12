bc4d3d0d4669ee106bae67539d8654e5e9acdde9
Aftab Sunny <blackparrotfedora@gmail.com>
2026-01-13 05:04:05 +0600

    Add automated messaging script using pyautogui

---
+++ main.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+
+message = [Message number]
+
+while message > 0;
+  time.sleep(3)
+  pyautogui.typewriter([Your message])
+  time.sleep(3)
+  pyautogui.press('enter)
+message = message - 1
